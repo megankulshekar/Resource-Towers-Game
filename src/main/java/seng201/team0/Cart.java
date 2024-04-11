db327e33ebb7e60ca7e0aee82ff2fd5279068417
Calan Meechang <calan.meechang@gmail.com>
2024-04-11 20:27:40 +1200

    Implemented Cart class

---
+++ src/main/java/seng201/team0/Cart.java
@@ -0,0 +1,98 @@
+package seng201.team0;
+
+/**
+ * Class representing a cart
+ */
+public class Cart {
+    /**
+     * Size of the cart
+     */
+    private int size;
+    /**
+     * The type of resource the cart can carry
+     */
+    private String resourceType;
+    /**
+     * Speed of the cart
+     */
+    private int speed;
+    /**
+     * Variable for showing if cart is full
+     */
+    private boolean full = false;
+    /**
+     * The current amount of resources in the cart
+     */
+    private int currentLevel = 0;
+
+    /**
+     * Constructor
+     * @param cartSize Size of cart
+     * @param cartResourceType Type of resource the cart can carry
+     * @param carSpeed Speed of the cart
+     */
+    public Cart(int cartSize, String cartResourceType, int carSpeed){
+        size = cartSize;
+        resourceType = cartResourceType;
+        speed = carSpeed;
+    }
+
+    /**
+     * Gets size of the cart
+     * @return Size of the cart
+     */
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * Gets the resource type of the cart
+     * @return Resource type of the cart
+     */
+    public String getResourceType(){
+        return resourceType;
+    }
+
+    /**
+     * Gets the speed of the cart
+     * @return Speed of the cart
+     */
+    public int getSpeed(){
+        return speed;
+    }
+
+    /**
+     * Gets whether cart if full
+     * @return Variable for showing if cart is full
+     */
+    public boolean getFull() {
+        return full;
+    }
+
+    /**
+     * Sets cart as full
+     */
+    public void setFullToTrue(){
+        full = true;
+    }
+
+    /**
+     * Gets the current amount of resources in the cart
+     * @return Current amount of resources in the cart
+     */
+    public int getCurrentLevel(){
+        return currentLevel;
+    }
+
+    /**
+     * Increases current amount of resources in the cart
+     * If current level is greater than or equal to size, cart is set to full
+     * @param increaseAmount Amount current level is increased by
+     */
+    public void increaseCurrentLevel(int increaseAmount){
+        currentLevel += increaseAmount;
+        if (currentLevel >= size){
+            setFullToTrue();
+        }
+    }
+}
